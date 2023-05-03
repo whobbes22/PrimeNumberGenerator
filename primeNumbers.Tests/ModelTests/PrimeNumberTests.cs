8ef9ced80c2997dace2aabcb81f6d3f232268bcc
Joe W <whobbes22@gmail.com>
2023-05-03 15:41:48 -0700

    completed test for prime numbers less then 100

--- primeNumbers.Tests/ModelTests/PrimeNumberTests.cs
+++ primeNumbers.Tests/ModelTests/PrimeNumberTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PrimeNumber.Models;
+using System.Collections.Generic;
 using System;
 
 namespace PrimeNumber.Tests
@@ -11,6 +12,60 @@ namespace PrimeNumber.Tests
     public void PrimeConstructor_CreateInstanceOfPrimeGenerator_PrimeGenerator()
     {
       PrimeGenerator num = new PrimeGenerator(300);
+      Assert.AreEqual(typeof(PrimeGenerator),num.GetType());
     }
+    [TestMethod]
+    public void PrimeList_CreateList_List()
+    {
+      PrimeGenerator num = new PrimeGenerator(10);
+      // make list
+      List<int> pList = num.CreateList();
+      List<int> testList = new List<int> {1,2,3,4,5,6,7,8,9,10};
+
+      for(int i = 0; i <=9 ;i++) 
+      {
+        Assert.AreEqual(pList[i], testList[i]); 
+      }
+    }
+
+    [TestMethod]
+    public void RemoveList_RemoveNonPrimeNumbers_List()
+    {
+      PrimeGenerator num = new PrimeGenerator(10);
+      List<int> pList = num.CreateList();
+      pList = num.KeepPrimeNumbers(pList);
+      List<int> testList = new List<int> {2,3,5,7};
+      Assert.AreEqual(pList[2],testList[2]);
+    }
+
+    [TestMethod]
+    public void RemoveList_edgeCase2_List()
+    {
+      PrimeGenerator num = new PrimeGenerator(2);
+      List<int> pList = num.CreateList();
+      pList = num.KeepPrimeNumbers(pList);
+      List<int> testList = new List<int> {2};
+      Assert.AreEqual(pList[0],testList[0]);
+    }
+
+    [TestMethod]
+    public void RemoveList_check100_List()
+    {
+      PrimeGenerator num = new PrimeGenerator(100);
+      List<int> pList = num.CreateList();
+      pList = num.KeepPrimeNumbers(pList);
+      List<int> testList = new List<int> {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+
+      for(int i = 0; i < pList.Count;i++){
+        Assert.AreEqual(pList[i],testList[i]);
+      }
+      // foreach(int element in pList)
+      // {
+      //   Assert.AreEqual(pList[element],testList[element]);
+      // }
+      
+    }
+
   }
 }
+
--- primeNumbers/Models/Numbers.cs
+++ primeNumbers/Models/Numbers.cs
@@ -1,15 +1,90 @@
+using System;
 using System.Collections.Generic;
 
 namespace PrimeNumber.Models
 {
   public class PrimeGenerator
   {
+    private int _stopIndex;
+    private List<int> _primeList = new List<int>{};
     public PrimeGenerator(int stop)
     {
-      
+      _stopIndex = stop;
+    }
+
+    public List<int> CreateList()
+    {
+      List<int> numberList = new List<int> {};
+      for(int i = 1; i <= _stopIndex; i++)
+      {
+        numberList.Add(i);
+        //Console.WriteLine(i);
+      }
+      return numberList;
+    }
+
+    public List<int> KeepPrimeNumbers(List<int> theList)
+    {
+      // 
+      theList.Remove(1);
+
+      if (_stopIndex == 2)
+      {
+        //return list with 2
+        //return list with 2
+      }
+
+      int index = 2;
+
+    // 1- 10
+    // input is 3
+    // get rid of 6 9 but DO NOT GET RID OF 3
+
+    // for loop, 
+    // the plan was to count the amount of things in the list
+      while(index < _stopIndex)
+      {
+        if(theList.Contains(index))
+        {
+          // i = 4; 4< 10; 3, 6,9..; 2, 4, 6, 8...;
+
+          // bool doNotDelete = true;
+          // really get rid of the first
+
+
+          //loop 2 skil 2 go to 4
+          //loop 3 skip 3 go to 6
+
+        //   2 cross all 2*  ; 3 all *3 ;4 index ++
+          for(int i = 2 * index; i <= _stopIndex; i = i + index)
+
+          {
+            // if(doNotDelete){
+            //   doNotDelete = false;
+            // }    <-get rid of the first loop which means the first number to be kept
+            // else{
+            Console.WriteLine($" i to be removed from list {i}, current multiple of {index}");
+            theList.Remove(i);          
+          }
+        }
+        index++;
+      }
+
+      foreach(int element in theList)
+      {
+        Console.WriteLine($"Current prime numbers in list {element}");
+      }
+      return theList;
     }
   }
 }
+
+
+// list of prime numbers under 100
+//2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+// current output
+//2, 3, 4, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 
 
 
 /*
